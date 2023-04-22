f08d97eff8acc828b299f05fab258b21434baf1c
Matthew Reynolds <mreynolds@student.sdccd.edu>
2023-04-22 09:14:28 -0700

    Made it so the server and the Client work together better Moved most of the classes to the common folder.

--- Client/src/main/java/edu/sdccd/cisc191/template/Client.java
+++ Client/src/main/java/edu/sdccd/cisc191/template/Client.java
@@ -1,48 +1,56 @@
 package edu.sdccd.cisc191.template;
 
-import java.io.*;
-import java.net.*;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.util.InputMismatchException;
+import java.util.Scanner;
 
 public class Client {
-    public static void main(String[] args) {
-        try {
-            // Connect to the server
-            Socket socket = new Socket("localhost", 8080);
-            System.out.println("Connected to server");
 
-            // Setup input/output streams
-            BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-            PrintWriter out = new PrintWriter(new OutputStreamWriter(socket.getOutputStream()), true);
-            BufferedReader consoleIn = new BufferedReader(new InputStreamReader(System.in));
+    // MAIN CLASS FOR THE PROGRAM
+    // Gathers all the data from Income, Expense, and Donations
 
-            // Send requests to the server
-            String userInput;
-            do {
-                // Display options to the client
-                System.out.println("Choose an option:");
-                System.out.println("1. Add income");
-                System.out.println("2. Add expense");
-                System.out.println("3. Add donation");
-                System.out.println("4. Generate financial report");
-                System.out.println("5. Quit");
+    // PROGRAM USES MODULE 1, 4, 5, 6, 7
+    public static void main(String[] args) throws Exception {
+        // Declares variables and allows for user to input the amounts (module 5)
+        Scanner sc = new Scanner(System.in);
+        Income income = new Income();
+        Expense expense = new Expense();
+        Donations donation = new Donations();
 
-                // Receive the user's choice from the console
-                int choice = Integer.parseInt(consoleIn.readLine());
+        // Connect to server
+        Socket socket = new Socket("localhost", 8080);
+        BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+        PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
 
-                // Send the user's choice to the server
-                out.println(choice);
+        boolean running = true;
+        while (running) {
+            // Displays all options
+            System.out.println("Choose an option:");
+            System.out.println("1. Add income");
+            System.out.println("2. Add expense");
+            System.out.println("3. Add donation");
+            System.out.println("4. Generate financial report");
+            System.out.println("5. Quit");
 
-                // Handle the user's choice
+            try {
+                int choice = sc.nextInt();
+                sc.nextLine();
+
                 switch (choice) {
                     case 1:
                         // Takes values from income
                         // Prints these values and adds them up.
                         System.out.println("Enter income amount:");
-                        double incomeAmount = Double.parseDouble(consoleIn.readLine());
+                        double incomeAmount = sc.nextDouble();
                         if (incomeAmount <= 0) {
                             System.out.println("Invalid input. Please enter a number greater than zero.");
                         } else {
+                            income.transaction(incomeAmount);
                             System.out.println("$" + incomeAmount + " added to income");
+                            System.out.println("Current income balance: $" + income.getTotalIncome());
                         }
                         break;
 
@@ -50,44 +58,81 @@ public class Client {
                         // Takes values from expenses
                         // Prints these values and adds them up, subtracts from income
                         System.out.println("Enter expense amount:");
-                        double expenseAmount = Double.parseDouble(consoleIn.readLine());
+                        double expenseAmount = sc.nextDouble();
                         if (expenseAmount <= 0) {
                             System.out.println("Invalid input. Please enter a number greater than zero.");
                         } else {
+                            expense.transaction(expenseAmount);
                             System.out.println("$" + expenseAmount + " added to expenses");
+                            System.out.println("Current expense balance: $" + expense.getTotalExpenses());
                         }
                         break;
 
                     case 3:
                         // Takes values from donations
-                        // Prints these values and adds them up.
+                        // Prints these values and adds them up, subtracts from income
+                        // Displays which individual charities you donated to, along with the amount for each
                         System.out.println("Enter donation amount:");
-                        double donationAmount = Double.parseDouble(consoleIn.readLine());
+                        double donationAmount = sc.nextDouble();
                         if (donationAmount <= 0) {
                             System.out.println("Invalid input. Please enter a number greater than zero.");
                         } else {
-                            System.out.println("$" + donationAmount + " added to donations");
+                            donation.transaction(donationAmount);
+                            System.out.println("$" + donationAmount + " donated to charity");
+                            System.out.println("Current donation balance: $" + donation.getTotalDonations());
                         }
                         break;
 
                     case 4:
-                        // Generate the financial report
-                        String report = in.readLine();
-                        System.out.println(report);
+                        // gets all values that were gathered
+                        double totalIncome = income.getTotalIncome();
+                        double totalExpenses = expense.getTotalExpenses();
+                        double totalDonations = donation.getTotalDonations();
+
+                        // Show individual totals, along with the grand balance
+                        System.out.println("Total income: $" + totalIncome);
+                        System.out.println("-----------");
+                        System.out.println("Total expenses: $" + totalExpenses);
+                        System.out.println("-----------");
+                        System.out.println("Total donations: $" + totalDonations);
+
+                        donation.printCharities();
+                        System.out.println("-----------");
+
+                        // Adds and subtracts all respective values and displays them
+                        double finalBalance = totalIncome - totalExpenses - totalDonations;
+                        double totalSpending = totalExpenses + totalDonations;
+
+                        // Final balance is displayed
+                        // Shows the status message of how the user is doing in regards to spending and saving
+                        System.out.println("Final balance: $" + finalBalance);
+                        if (finalBalance < totalSpending) {
+                            System.out.println("You are spending more than you are earning. Consider reducing expenses or increasing income.");
+                        } else if (finalBalance > totalSpending) {
+                            System.out.println("You are saving more than you are spending. Good job!");
+                        } else if (finalBalance < 0) {
+                            System.out.println("You have an overdraft. You may be subject to overdraft fees.");
+                        }
                         break;
 
                     case 5:
-                        // Exit the loop
-                        System.out.println("Goodbye!");
+                        // Exits program
+                        running = false;
                         break;
 
                     default:
-                        System.out.println("Invalid option. Please choose a number from 1 to 5.");
-                        break;
+                        // invalid choice, shows menu again
+                        System.out.println("Invalid choice.");
                 }
-            } while (true);
-        } catch (IOException e) {
-            e.printStackTrace();
+            }
+            catch (InputMismatchException e) {
+                // if a letter or special character is included in the input, this exception is handled
+                // it prompts the user to enter a valid input
+                // only integers are allowed when the program asks which choice
+
+                System.out.println("Invalid input. Please enter a number (integer or two decimals).");
+                sc.next();
+            }
         }
     }
 }
